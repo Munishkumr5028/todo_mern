95c3b133e9c52590e2fe7f5818666d099f9e7274
Munishkumr5028 <160215415+Munishkumr5028@users.noreply.github.com>
2025-06-18 13:28:55 +0530

    Update vite.config.js

--- _client/vite.config.js
+++ _client/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  preview: {
+    port: 5000,
+    host: true,
+    allowedHosts: ['todo-app-mern.up.railway.app']
+}
 })
